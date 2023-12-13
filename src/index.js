99123862ae74636c02c4f9ef3fa70ddc7c5eca73
koki131 <zoran.stevanovich94@gmail.com>
2023-12-13 15:54:49 +0100

    add n-knights

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import "./style.css";
 import nQueens from "./nQueens";
 import solveSudoku from "./sudokuSolver";
+import nKnights from "./nKnights";
 
 
 
 const nQueensContainer = document.querySelector(".n-queens-container");
+const nKnightsContainer = document.querySelector(".n-knights-container");
 const sudokuContainer = document.querySelector(".sudoku-container");
 const selectProblem = document.querySelector("#select-problem");
 
@@ -16,10 +18,17 @@ selectProblem.addEventListener("change", function(event) {
         case "N-Queens":
             sudokuContainer.style.display = "none";
             nQueensContainer.style = "display: grid; grid-template: 1fr / 10fr 3fr; align-items: center; justify-items: center;";
+            nKnightsContainer.style.display = "none";
             break;
         case "Sudoku":
             sudokuContainer.style.display = "flex";
             nQueensContainer.style.display = "none";
+            nKnightsContainer.style.display = "none";
+            break;
+        case "N-Knights":
+            nKnightsContainer.style = "display: grid; grid-template: 1fr / 10fr 3fr; align-items: center; justify-items: center;"
+            nQueensContainer.style.display = "none";
+            sudokuContainer.style.display = "none";
             break;
     }
 
