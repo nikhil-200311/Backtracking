896c8773b115f42183fdda29315ee1b007e5eaa4
koki131 <zoran.stevanovich94@gmail.com>
2023-12-15 23:35:10 +0100

    add word search

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import "./style.css";
 import nQueens from "./nQueens";
 import solveSudoku from "./sudokuSolver";
 import nKnights from "./nKnights";
+import wordSearch from "./wordSearch";
 
 
 
+const wordSearchContainer = document.querySelector(".word-search-container");
 const nQueensContainer = document.querySelector(".n-queens-container");
 const nKnightsContainer = document.querySelector(".n-knights-container");
 const sudokuContainer = document.querySelector(".sudoku-container");
@@ -19,16 +21,25 @@ selectProblem.addEventListener("change", function(event) {
             sudokuContainer.style.display = "none";
             nQueensContainer.style = "display: grid; grid-template: 1fr / 10fr 3fr; align-items: center; justify-items: center;";
             nKnightsContainer.style.display = "none";
+            wordSearchContainer.style.display = "none";
             break;
         case "Sudoku":
             sudokuContainer.style.display = "flex";
             nQueensContainer.style.display = "none";
             nKnightsContainer.style.display = "none";
+            wordSearchContainer.style.display = "none";
             break;
         case "N-Knights":
             nKnightsContainer.style = "display: grid; grid-template: 1fr / 10fr 3fr; align-items: center; justify-items: center;"
             nQueensContainer.style.display = "none";
             sudokuContainer.style.display = "none";
+            wordSearchContainer.style.display = "none";
+            break;
+        case "Word-Search":
+            wordSearchContainer.style.display = "flex";
+            nQueensContainer.style.display = "none";
+            sudokuContainer.style.display = "none";
+            nKnightsContainer.style.display = "none";
             break;
     }
 
---
+++ src/wordSearch.js
@@ -0,0 +1,178 @@
+const searchBoard = document.querySelector("#search-board");
+const searchForWord = document.querySelector("#find-word");
+const wordToFind = document.querySelector("#word-to-find");
+const generateBoard = document.querySelector("#generate-word-board");
+const searchSpeed = document.querySelector("#search-speed");
+
+let value = 300;
+let hasFinished = true;
+
+let board = [];
+
+
+searchSpeed.addEventListener("change", function(event) {
+
+    switch(event.target.value) {
+        case "Slow":
+            value = 300;
+            break;
+        case "Medium":
+            value = 150;
+            break;
+        case "Fast":
+            value = 0;
+            break;
+    }
+
+})
+
+
+wordSearch();
+
+export default function wordSearch() {
+
+    
+    displayBoard();
+
+    generateBoard.addEventListener("click", function() {
+        if (hasFinished) {
+            searchBoard.textContent = "";
+            displayBoard();
+        }
+
+    });
+
+
+    if (board) {
+
+        searchForWord.addEventListener("click", async function() {
+            
+            if (wordToFind.value != "") {
+                hasFinished = false;
+                const found = await findStartAndEnd(board);
+                hasFinished = true;
+                console.log(found);
+            
+            }
+
+        });
+
+
+    }
+
+}
+
+async function findStartAndEnd(board) {
+
+    const word = wordToFind.value.toUpperCase();
+
+    const ch = word.charAt(0);
+
+    
+
+    let visited = [];
+
+    for (let i = 0; i < board.length; i++) {
+        
+        visited[i] = [false, false, false, false, false, false, false, false, false, false];
+    }
+
+    for (let r = 0; r < board.length; r++) {
+        for (let c = 0; c < board[r].length; c++) {
+
+            if (board[r][c].textContent === ch) {
+
+                const found = await findWord(board, visited, word,  r, c, 0);
+                
+                if (found) {
+                    return true;
+                }
+                
+            }
+        }
+    }
+
+    return false;
+
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function findWord(board, visited, word, r, c, idx) {
+
+    if (idx === word.length) {
+
+        return true;
+    }
+
+    if (r < 0 || c < 0) {
+        return false;
+    } 
+
+    if (r > board.length-1 || c > board[r].length-1) {
+        return false;
+    }
+    if (word.charAt(idx) === board[r][c].textContent) {
+        await sleep(value);
+        board[r][c].style.backgroundColor = "green";
+    }
+
+    if (word.charAt(idx) !== board[r][c].textContent) {
+        await sleep(value);
+        if (board[r][c].style.backgroundColor !== "green") {
+            board[r][c].style.backgroundColor = "red";
+        }
+        return false;
+    }
+    
+    if (visited[r][c]) {
+        return false;
+    }
+
+    visited[r][c] = true;
+
+
+    await sleep(value);
+
+    let isWord =  await findWord(board, visited, word, r + 1, c, idx + 1) ||
+
+                  await findWord(board, visited, word, r - 1, c, idx + 1) ||
+
+                  await findWord(board, visited, word, r, c + 1, idx + 1) ||
+  
+                  await findWord(board, visited, word, r, c - 1, idx + 1);
+    await sleep(value);
+    
+    if (!isWord) {
+        board[r][c].style.backgroundColor = "white";
+    }
+
+
+    visited[r][c] = false;
+
+    return isWord;
+
+}
+
+
+
+function displayBoard() {
+    
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    for (let r = 0; r < 9; r++) {
+        board[r] = [];
+        for (let c = 0; c < 9; c++) {
+            const index = Math.floor(Math.random() * characters.length);
+            const div = document.createElement("div");
+            board[r][c] = div;
+            div.textContent = characters[index];
+            searchBoard.append(div);
+
+        }
+    }
+
+
+}
